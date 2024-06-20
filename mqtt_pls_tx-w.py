ba1925728c2ed63f84b167ff4ada06b1b3bdca8f
jeff lee <121525685+jefflee2022@users.noreply.github.com>
2024-06-21 08:09:34 +0900

    Update mqtt_pls_tx-w.py

--- mqtt_pls_tx-w.py
+++ mqtt_pls_tx-w.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This is a sample Python script for MQTT test 
-$ TX62-W(B) & (m)PLS-62 modules was tested .
+# TX62-W(B) & (m)PLS-62 modules was tested .
 # scripted by jefflee@ocube.co.kr
 
 # Press Shift+F10 to execute it or replace it with your code.
